92e364b144bdd6d2a36379c98bc20c34defff8a0
pedrofaino <pedrofaino99@gmail.com>
2022-07-29 02:30:51 -0300

    secod commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './home/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { BodyComponent } from './home/body/body.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ButtonComponent } from './componentes/button/button.component';
+import { CardComponent } from './componentes/card/card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    NavComponent,
+    BodyComponent,
+    ButtonComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
