e951b52e697b8214960cfe8f60fe9790fb23219f
pedrofaino <pedrofaino99@gmail.com>
2022-10-04 19:03:48 -0300

    proyecto terminado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
   apiBaseUrl:"https://portfoliopf.herokuapp.com/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
-  production: true,
+  production: false,
   apiBaseUrl:"http://localhost:8080/"
 };
